64f0c24bd8fb50b35278420879400b602bf3ac5b
Martin Six <if25b050@technikum-wien.at>
2026-06-09 16:17:41 +0200

    Implement Failing Unit-Test

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  modulePathIgnorePatterns: [
+    "<rootDir>/dist/",
+    "<rootDir>/node_modules/"
+  ],
+};

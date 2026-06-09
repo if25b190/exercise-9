70667e358cce49379f1412d70c24aaba544d26ba
Schnitzel5 <tangphilip3@gmail.com>
2026-06-09 16:07:07 +0200

    init

---
+++ app/database/prisma/migrations/20250220100218_create_counter/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "counters" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "counters_pkey" PRIMARY KEY ("id")
+);
